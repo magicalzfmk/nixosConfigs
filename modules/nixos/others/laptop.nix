b408b4fe641487afdf7905bbc8ba4a6c5960f4aa
zfmk <mitanshkataria73@gmail.com>
2024-10-05 22:44:09 +0530

    modified battery services (auto cpufreq -> tlp)

--- modules/nixos/others/laptop.nix
+++ modules/nixos/others/laptop.nix
@@ -1,23 +1,33 @@
 { config, pkgs, lib, ... }:
 {
   services.thermald.enable = true;
+  powerManagement.powertop.enable = true;
+  services.system76-scheduler.settings.cfsProfiles.enable = true;
   services.power-profiles-daemon.enable = lib.mkForce false;
   
-  #services.tlp.enable = true;
-  
-  services.auto-cpufreq = {
+  services.tlp = {
     enable = true;
     settings = {
-      battery = {
-        governor = "powersave";
-        turbo = "never";
-      };
-      charger = {
-        governor = "performance";
-        turbo = "auto";
-      };
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 0;
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
     };
   };
+  
+  #services.auto-cpufreq = {
+  #  enable = true;
+  #  settings = {
+  #    battery = {
+  #      governor = "powersave";
+  #      turbo = "never";
+  #    };
+  #    charger = {
+  #      governor = "performance";
+  #      turbo = "auto";
+  #    };
+  #  };
+  #};
   
   # Enable Opengl
   hardware.graphics = {
